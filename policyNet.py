b5de60691fa7c4b0ee4263ce604b7a83429e4740
Zhuoran Yang <zhuoranyang@zhuoran.home>
2017-08-15 14:06:34 -0400

    add the fundamental files

---
+++ policyNet.py
@@ -0,0 +1,10 @@
+"""
+
+This file contains the class of policy network pi(s,theta), which maps the state to a probablity distribution over the action space. Here s is the state, theta is the network parameter. 
+
+We focus on the problems where the action space is finte.
+
+"""
+
+
+ 
---
+++ valueNet.py
@@ -0,0 +1,60 @@
+"""
+
+Define a Value network Q(s,a,theta) where 
+    s is the state,
+    a is the action,
+    theta is the network parameter.
+
+Here we focus on the problems with finite action space.
+
+"""
+
+import numpy as np
+import tensorflow as tf
+
+class ValueNet(object):
+    def __init__(
+                self,
+n_actions,
+            n_features,
+            learning_rate=0.005,
+            reward_decay=0.9,
+            e_greedy=0.9,
+            replace_target_iter=200,
+            memory_size=3000,
+            batch_size=32,
+            e_greedy_increment=None,
+            output_graph=False,
+            double_q=True,
+            sess=None,
+):
+        self.n_actions = n_actions
+        self.n_features = n_features
+        self.lr = learning_rate
+        self.gamma = reward_decay
+        self.epsilon_max = e_greedy
+        self.replace_target_iter = replace_target_iter
+        self.memory_size = memory_size
+        self.batch_size = batch_size
+        self.epsilon_increment = e_greedy_increment
+        self.epsilon = 0 if e_greedy_increment is not None else self.epsilon_max
+
+        self.double_q = double_q    # decide to use double q or not
+
+        self.learn_step_counter = 0
+        self.memory = np.zeros((self.memory_size, n_features*2+2))
+        self._build_net()
+        t_params = tf.get_collection('target_net_params')
+        e_params = tf.get_collection('eval_net_params')
+        self.replace_target_op = [tf.assign(t, e) for t, e in zip(t_params, e_params)]
+
+        if sess is None:
+            self.sess = tf.Session()
+            self.sess.run(tf.global_variables_initializer())
+        else:
+            self.sess = sess
+        if output_graph:
+            tf.summary.FileWriter("logs/", self.sess.graph)
+        self.cost_his = []
+
+
